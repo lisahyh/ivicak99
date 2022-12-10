be5cf704bc03826df74443c5bd4b73e23dd0d930
lisahyh <120273385+lisahyh@users.noreply.github.com>
2022-12-10 18:54:43 +0100

    Update aufgabe_2_abcd.hs

--- Übung 7/aufgabe_2_abcd.hs
+++ Übung 7/aufgabe_2_abcd.hs
@@ -9,3 +9,8 @@ module Aufgabe2 where
     lowhigh :: [Int] -> (Int,Int)
     lowhigh [] = (0,0)
     lowhigh xs = (length(filter (>0) xs), length(filter (>0) xs))
+
+f x = x*x-5
+lowhighfunc :: [Int] -> (Int,Int)
+lowhighfunc [] = 0
+lowhighfunc (x:xs) = f x:lhf xs
